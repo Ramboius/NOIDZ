6c7b3e8ca708c9a2092ecaf1a6d727780f01eb98
Luis Custodio <lcustodio@89gr.pt>
2013-10-18 23:14:04 +0100

    App Added 3

--- bin/noidz_app-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/Custodio/Desktop/android-noidz/bin/noidz_app-debug-unaligned.apk : \
-/Users/Custodio/Desktop/android-noidz/bin/noidz_app.ap_ \
-/Users/Custodio/Desktop/android-noidz/bin/classes.dex \
-/Users/Custodio/Desktop/android-noidz/libs/YouTubeAndroidPlayerApi.jar \
-/Users/Custodio/Desktop/android-noidz/libs/cordova-2.9.0.jar \
